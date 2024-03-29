bec3066dfafc2824cbdc146a0c75421c96720be6
seanboss-bit <shaunaibek@gmail.com>
2024-03-29 12:14:59 +0100

    back url

--- src/request.js
+++ src/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/";
+const BASE_URL = "https://zen-back-xr96.onrender.com/";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
